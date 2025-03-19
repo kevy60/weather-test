8b65535fcf21bdae2ecab4986968ab4e3b6150cb
kevy60 <kevin.vorsmann@voco.ee>
2025-03-19 08:40:29 +0200

    Component testimine - cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
+const webpackPreprocessor = require('@cypress/webpack-preprocessor');
+const webpackConfig = require('./webpack.config');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      on('file:preprocessor', webpackPreprocessor({ webpackOptions: webpackConfig }));
     },
-    baseUrl: 'http://localhost:3001', 
+    baseUrl: "http://localhost:3000",
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+      webpackConfig,
+    },
   },
 });
